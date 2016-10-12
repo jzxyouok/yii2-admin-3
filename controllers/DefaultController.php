e67c500ceceac003dc5fe043b9e5983f00a36ae2
Yang <purocean@gmail.com>
2016-10-12 20:04:02 +0800

    Copy files form mdmsoft/yii2-admin

---
+++ controllers/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace purocean\yii2admin\controllers;
+
+use Yii;
+
+/**
+ * DefaultController
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
+ */
+class DefaultController extends \yii\web\Controller
+{
+
+    /**
+     * Action index
+     */
+    public function actionIndex($page = 'README.md')
+    {
+        die(1);
+        if (strpos($page, '.png') !== false) {
+            $file = Yii::getAlias("@purocean/yii2admin/{$page}");
+            return Yii::$app->getResponse()->sendFile($file);
+        }
+        return $this->render('index', ['page' => $page]);
+    }
+}
